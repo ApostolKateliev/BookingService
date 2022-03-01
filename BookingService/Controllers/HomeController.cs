032aec2b4fabfedf1923f4aa964a7c3aa1981d6b
apostol <apostol.atanasov91@gmail.com>
2022-03-01 16:54:45 +0200

    Add project files.

---
+++ BookingService/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BookingService.Core.Constants;
+using BookingService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookingService.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData[MessageConstant.ErrorMessage] = "Something went wrong!";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookingService/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingService.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
