41aa901cda3f04c1d673d35e9d80ba2f122f418f
apostol <apostol.atanasov91@gmail.com>
2022-03-04 21:24:52 +0200

    Added Model binders

--- BookingService/Program.cs
+++ BookingService/Program.cs
@@ -1,4 +1,6 @@
+using BookingService.Core.Constants;
 using BookingService.Data;
+using BookingService.ModelBinders;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,7 +14,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddMvcOptions(options =>
+    {
+        options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+        options.ModelBinderProviders.Insert(1, new DateTimeModelBinderProvider(FormatingConstant.NormalDateFormat));
+    });
 
 var app = builder.Build();
 
