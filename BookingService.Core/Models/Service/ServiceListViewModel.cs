bbad3ef7a13aa78aa4e3a9a213d417036f982ee4
apostol <apostol.atanasov91@gmail.com>
2022-04-23 19:54:25 +0300

    edited some views

--- BookingService.Core/Models/Service/ServiceListViewModel.cs
+++ BookingService.Core/Models/Service/ServiceListViewModel.cs
@@ -11,11 +11,11 @@ namespace BookingService.Core.Models.Service
        
         public string Id { get; set; }
 
-        
         public string? Name { get; set; }
         public string? Duration { get; set; }
         public string? Price { get; set; }
 
+        public string? Description { get; set; }
 
     }
 }
