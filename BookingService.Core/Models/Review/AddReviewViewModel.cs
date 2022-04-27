344a619b97cee9c30db7ec887df2d34feb3f7a42
apostol <apostol.atanasov91@gmail.com>
2022-04-28 00:44:14 +0300

    added review

--- BookingService.Core/Models/Review/AddReviewViewModel.cs
+++ BookingService.Core/Models/Review/AddReviewViewModel.cs
@@ -10,9 +10,13 @@ namespace BookingService.Core.Models.Review
     public class AddReviewViewModel
     {
         [Required]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+
         public string? Name { get; set; }
 
         [Required]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
+
         public string? Body { get; set; }
     }
 }
---
+++ BookingService/Controllers/ReviewController.cs
@@ -0,0 +1,31 @@
+﻿using BookingService.Core.Contracts;
+using BookingService.Core.Models.Review;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.Controllers
+{
+    public class ReviewController : BaseController
+    {
+        private readonly IReviewService service;
+
+        public ReviewController(IReviewService _service)
+        {
+            service = _service;
+        }
+
+        public IActionResult AddReview()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddReview(AddReviewViewModel model)
+        {
+
+            await service.AddReview(model);
+
+            ModelState.Clear();
+            return View(model);
+        }
+    }
+}
